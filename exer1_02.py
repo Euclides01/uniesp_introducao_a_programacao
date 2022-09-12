42327b2dabd2a0ad0e3e82d5af521dcc2e9df8e0
Euclides Costa Benício Neto <112564209+Euclides01@users.noreply.github.com>
2022-09-12 17:07:11 -0300

    Delete exer1_02.py

--- exer1_02.py
+++
@@ -1,4 +0,0 @@
-#Crie um programa em Python que pergunte o nome do usuário e imprima um bom dia com o nome do usuário. Dica: você pode utilizar o método .format() ou uma concatenação de string, por exemplo.
-
-nome = input("Olá, digite o seu nome por favor: ")
-print(f"Bom dia {nome}")
