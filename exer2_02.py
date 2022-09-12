1e3bc64be4f60e26fb1dc7826773ad4fa5bb3472
Euclides Costa Benício Neto <112564209+Euclides01@users.noreply.github.com>
2022-09-12 17:08:18 -0300

    Delete exer2_02.py

--- exer2_02.py
+++
@@ -1,9 +0,0 @@
-#Solicite ao usuário um valor numérico, inteiro ou real, e escrever se é positivo ou negativo (considere o valor zero como positivo).
-
-numero = float(input("Digite um número: "))
-if (numero < 0):
-    print(f"{numero} é negativo")
-
-else:
-    print(f"{numero} é positivo")
-
