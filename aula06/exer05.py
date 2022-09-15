530bbdf777c64f21ede20105dfe9ea9d859db6ce
Euclides Costa Benício Neto <112564209+Euclides01@users.noreply.github.com>
2022-09-15 16:42:30 -0300

    Delete exer05.py

--- aula06/exer05.py
+++
@@ -1,2 +0,0 @@
-#Utilizando a lista do exercício anterior, exiba uma saudação ("Olá como vai você"), personalizado
-#com o nome de cada amigo. A saudação deve ser a mesma, alterando apenas o nome do amigo.
